9511092c4ec5e9ce4b18f6325a96f9129e2ec2cd
matheusdeveloperphp <matheus.lima@blueservice.com.br>
2026-02-23 18:49:30 -0300

    Instalando e configurando o RabbitMQ

--- config/queue.php
+++ config/queue.php
@@ -67,6 +67,28 @@ return [
             'block_for' => null,
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+
+            'options' => [
+                'exchange' => [
+                    'name' => env('RABBITMQ_EXCHANGE_NAME'),
+                    'type' => 'direct',
+                ],
+            ],
+        ],
+
     ],
 
     /*
