455098bdebe91d78a5917de94d993f8c2ebbf117
matheusdeveloperphp <matheus.lima@blueservice.com.br>
2026-02-23 11:27:04 -0300

    CoIniciando a configuração incial do projeto

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/' , function(){
+   return response()->json(['message' => 'Welcome to my API!']);
+});
